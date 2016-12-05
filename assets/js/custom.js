683d41b5952d5ac3bb08c40afdd074e0d95208d7
Student <student@BSD-02.local>
2016-12-05 17:26:17 -0500

    12/5/16

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,19 @@ function flippy( elem ) {
   }
  
 }
+
+
+
+function dispProg() {
+  	// body...
+    var progress = document.getElementById("innerprog");
+   // var debug = document.getElementById("debug")
+  
+  var target = progress.dataset.target;
+  var prog = progress.dataset.progress;
+  var complete = 1 - (prog/target);
+    // debug.innerHTML = complete;
+  progress.style.width = complete*100+"%";
+      progress.innerHTML = "$" + prog;
+  // progress.style.backgroundColor = "green";
+}
