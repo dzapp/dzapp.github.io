5b5d0c0c8b67ebe15e763e27bd5de723d0bde71c
xaviersweb1876520 <student@BSD-03.local>
2017-01-23 17:24:46 -0500

    slide show

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,3 +26,19 @@ function dispProg() {
       progress.innerHTML = "$" + prog;
   // progress.style.backgroundColor = "green";
 }
+function next() {
+  var frame = document.getElementById("slider");
+  if( frame.classList == ""){
+      frame.classList = "two";
+  } else if(frame.classList == "two"){
+     frame.classList = "three";
+  } else if (frame.classList == "three"){
+    frame.classList = "four";
+  } else if (frame.classList == "four"){
+    frame.classList = "";
+  }
+
+}
+setInterval(next, 2000); // Time in milliseconds
+
+// var intViewportHeight = window.innerHeight;
